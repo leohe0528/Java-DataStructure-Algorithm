ce886f287d32066741a0f99dfcee7d429e5d918d
leohe0528 <leohe0528@g.ucla.edu>
2019-07-18 14:48:08 -0700

    added Palindrome Partitioning II

---
+++ src/DynamicProgramming/PalindromePartitioningII.java
@@ -0,0 +1,26 @@
+package DynamicProgramming;
+
+public class PalindromePartitioningII {
+    public int minCut(String s) {
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        boolean[][] isPal = new boolean[len][len];
+
+        for (int i = len - 1; i >= 0; i--){
+            dp[i] = len - i;
+            for (int j = i; j < len; j++){
+                if (s.charAt(i) == s.charAt(j) &&
+                        (i == j || i + 1 == j ||
+                                isPal[i + 1][j - 1])){
+                    dp[i] = Math.min(dp[j + 1] + 1, dp[i]);
+                    isPal[i][j] = true;
+                }
+            }
+        }
+        return dp[0] - 1;
+    }
+    public static void main(String[] args){
+        String a = "aab";
+        int res = new PalindromePartitioningII().minCut(a);
+    }
+}
